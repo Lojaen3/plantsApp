f083decb7fc26d6e8bac63941364ef8a40ffda86
Lojaen3 <lojaenayash@gmail.com>
2025-10-19 09:06:32 +0300

    Initial Commit

---
+++ plants/plantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantsApp.swift
+//  plants
+//
+//  Created by Lojaen Jehad Ayash on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plantsTests/plantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  plantsTests.swift
+//  plantsTests
+//
+//  Created by Lojaen Jehad Ayash on 27/04/1447 AH.
+//
+
+import Testing
+@testable import plants
+
+struct plantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
